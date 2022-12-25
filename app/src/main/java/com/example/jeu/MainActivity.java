96504c41f061ba420fd85096b9f99e6e2970bb3b
mohamedd <djamila.mohamed@etu.u-paris.fr>
2022-12-25 09:17:28 +0100

    2 threads enlevés l code est inséré dans les listeners

--- app/src/main/java/com/example/jeu/MainActivity.java
+++ app/src/main/java/com/example/jeu/MainActivity.java
@@ -28,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
     private ImageView[] bugs_left = new ImageView[5];  // ensemble des insectes &agrave; gauche
     private int score_value = 0;                       // valeur du score sous forme d'entier pour faciliter les calculs et garder une sauvegarde du score courant
     private int life_value = 10;                       // points de vie du joueur
-    private boolean b1isPressed, b2isPressed;          // variables partagées entre les threads et les listeners
     private Lock lock = new ReentrantLock();
 
     @SuppressLint("ClickableViewAccessibility")
@@ -45,7 +44,12 @@ public class MainActivity extends AppCompatActivity {
         /* initialisation des images et des textes du jeu */
         gard = findViewById(R.id.gardener_rest);
         gard2 = findViewById(R.id.gardener_hit_left);
+        // ses coordonnées deviennent celles de son centre pour faciliter les calculs
+        gard2.setX(gard2.getWidth()/2);
+        gard2.setY(gard2.getHeight()/2);
         gard3 = findViewById(R.id.gardener_hit_right);
+        gard3.setX(gard3.getWidth()/2);
+        gard3.setY(gard3.getHeight()/2);
         plant1 = findViewById(R.id.plant1);
         plant2 = findViewById(R.id.plant2);
         b1 = findViewById(R.id.hit_left);
@@ -60,8 +64,12 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 5; i++) {
             bugs_right[i] = findViewById(R.id.bug);
             bugs_right[i].setVisibility(View.VISIBLE);
+            bugs_right[i].setX(bugs_right[i].getWidth()/2);
+            bugs_right[i].setY(bugs_right[i].getHeight()/2);
             bugs_left[i] = findViewById(R.id.bug2);
             bugs_left[i].setVisibility(View.VISIBLE);
+            bugs_left[i].setX(bugs_left[i].getWidth()/2);
+            bugs_left[i].setY(bugs_left[i].getHeight()/2);
         }
 
     }
@@ -79,115 +87,42 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     while (true) {
                         // todo : faire un id dans activity_main pour chaque insecte ? ici ils partagent tous le même view dans activity_main.xml
-                        for (int i = 0; i < 5; i++) {
-                            bugs_right[i].setY(bugs_right[i].getY() + 10);
-                            bugs_left[i].setY(bugs_left[i].getY() + 10);
+                       // for (int i = 0; i < 5; i++) {
+                            bugs_right[0].setY(bugs_right[0].getY() + 10);
+                            bugs_left[0].setY(bugs_left[0].getY() + 10);
                             sleep(100);
-                        }
+                       // }
                     }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
         };
-        //thread.start();
+        thread.start();
 
         /* thread pour évaluer les différents cas et calculer le score de la colonne de gauche */
         // todo : bug : les pv commencent à des valeurs aléatoires négatives, la boucle while incrémente trop les compteurs
-        Thread thread2 = new Thread() {
+        /*Thread thread2 = new Thread() {
             @Override
             public void run() {
                 while (true) {
-                    for (int i = 0; i < 5; i++) {
-                        if (b1isPressed) {
-                            float dist = getDist(bugs_left[i], gard2);
-                            Log.e("asd", "DISTANCE 1 = " + dist);
-                            /* cas où l'insecte est frappé mais pas correctement */
-                            if (dist < 300 && dist > 200) {
-                                Log.e("asd", "GOOD");
-                                int finalI = i;
-                                runOnUiThread(() -> bugs_left[finalI].setVisibility(View.INVISIBLE));
-                                runOnUiThread(() -> t2.setVisibility(View.VISIBLE));
-                                score_value += 5;
-                                runOnUiThread(() -> score.setText(String.valueOf(score_value)));
-                            }
-                            /* cas où l'insecte est frappé au bon moment */
-                            else if (dist <= 200) {
-                                Log.e("asd", "EXCELLENT");
-                                int finalI1 = i;
-                                runOnUiThread(() -> bugs_left[finalI1].setVisibility(View.INVISIBLE));
-                                runOnUiThread(() -> t1.setVisibility(View.VISIBLE));
-                                score_value += 10;
-                                runOnUiThread(() -> score.setText(String.valueOf(score_value)));
-                            }
-                            /* cas où on frappe à côté */
-                            else if (dist >= 300) {
-                                Log.e("asd", "MISS");
 
-                                runOnUiThread(() -> t3.setVisibility(View.VISIBLE));
-                                life_value -= 1;
-                                runOnUiThread(() -> life.setText(String.valueOf(life_value)));
-                                // todo : écrire la fonction de pénalité de temps lorsqu'on rate
-                            }
-                        }
-                        runOnUiThread(() -> t1.setVisibility(View.INVISIBLE));
-                        runOnUiThread(() -> t2.setVisibility(View.INVISIBLE));
-                        runOnUiThread(() -> t3.setVisibility(View.INVISIBLE));
-
-                        boolean isAttacking = getDist(bugs_right[i], plant1) < 10;
-                        if (isAttacking) {
-                            life_value--;
-                            runOnUiThread(() -> life.setText(String.valueOf(life_value)));
-                        }
-                    }
                 }
             }
         };
-        thread2.start();
+        thread2.start();*/
+
 
         /* thread pour évaluer les différents cas et calculer le score de la colonne de droite */
-        Thread thread3 = new Thread() {
+        /*Thread thread3 = new Thread() {
             @Override
             public void run() {
                 while (true) {
-                    for (int i = 0; i < 5; i++) {
-                        if (b2isPressed) {
-                            float dist = getDist(gard3, bugs_right[i]);
-                            Log.e("asd", "DISTANCE 2 = " + dist);
-                            if (dist < 300 && dist > 200) {
-                                Log.e("asd", "GOOD");
-                                int finalI1 = i;
-                                runOnUiThread(() -> bugs_right[finalI1].setVisibility(View.INVISIBLE));
-                                runOnUiThread(() -> t2.setVisibility(View.VISIBLE));
-                                score_value += 5;
-                                runOnUiThread(() -> score.setText(String.valueOf(score_value)));
-                            } else if (dist <= 200) {
-                                Log.e("asd", "EXCELLENT");
-                                int finalI = i;
-                                runOnUiThread(() -> bugs_right[finalI].setVisibility(View.INVISIBLE));
-                                runOnUiThread(() -> t1.setVisibility(View.VISIBLE));
-                                score_value += 10;
-                                runOnUiThread(() -> score.setText(String.valueOf(score_value)));
-                            } else if (dist >= 300) {
-                                Log.e("asd", "MISS");
-                                runOnUiThread(() -> t3.setVisibility(View.VISIBLE));
-                                life_value -= 1;
-                                runOnUiThread(() -> life.setText(String.valueOf(life_value)));
-                            }
-                        }
-                        runOnUiThread(() -> t1.setVisibility(View.INVISIBLE));
-                        runOnUiThread(() -> t2.setVisibility(View.INVISIBLE));
-                        runOnUiThread(() -> t3.setVisibility(View.INVISIBLE));
-                        boolean isAttacking = getDist(bugs_right[i], plant2) < 10;
-                        if (isAttacking) {
-                            life_value--;
-                            runOnUiThread(() -> life.setText(String.valueOf(life_value)));
-                        }
-                    }
+
                 }
             }
         };
-        thread3.start();
+        thread3.start();*/
 
         // todo : implémenter le GameOver lorsqu'on a perdu (vie <= 0)
 
@@ -195,16 +130,51 @@ public class MainActivity extends AppCompatActivity {
         b1.setOnTouchListener((v, event) -> {
             int action = event.getAction();
             if (action == MotionEvent.ACTION_DOWN) {
-                b1isPressed = true;
-                /* on passe d'une jardinière au repos à une jardinière qui frappe à gauche */
                 gard.setVisibility(View.INVISIBLE);
                 gard2.setVisibility(View.VISIBLE);
+                //for (int i = 0; i < 5; i++) {
+                    float dist = getDist(bugs_left[0], gard2);
+                    Log.e("asd", "DISTANCE 1 = " + dist);
+                    /* cas où l'insecte est frappé mais pas correctement */
+                    if (dist < 150 && dist > 100) {
+                        Log.e("asd", "GOOD");
+                        //int finalI = i;
+                        bugs_left[0].setVisibility(View.INVISIBLE);
+                        t2.setVisibility(View.VISIBLE);
+                        score_value += 5;
+                        score.setText(String.valueOf(score_value));
+                    }
+                    /* cas où l'insecte est frappé au bon moment */
+                    else if (dist <= 100) {
+                        Log.e("asd", "EXCELLENT");
+                        //int finalI1 = i;
+                        bugs_left[0].setVisibility(View.INVISIBLE);
+                        t1.setVisibility(View.VISIBLE);
+                        score_value += 10;
+                        score.setText(String.valueOf(score_value));
+                    }
+                    /* cas où on frappe à côté */
+                    else if (dist >= 150) {
+                        Log.e("asd", "MISS");
+                        t3.setVisibility(View.VISIBLE);
+                        life_value -= 1;
+                        life.setText(String.valueOf(life_value));
+                        // todo : écrire la fonction de pénalité de temps lorsqu'on rate
+                    }
+                    boolean isAttacking = getDist(bugs_right[0], plant1) < 10;
+                    if (isAttacking) {
+                        life_value--;
+                        life.setText(String.valueOf(life_value));
+                    }
+              //  }
                 return true;
             } else if (action == MotionEvent.ACTION_UP) {
-                b1isPressed = false;
-                /* la jardinière se met au repos */
                 gard2.setVisibility(View.INVISIBLE);
                 gard.setVisibility(View.VISIBLE);
+                t1.setVisibility(View.INVISIBLE);
+                t2.setVisibility(View.INVISIBLE);
+                t3.setVisibility(View.INVISIBLE);
+                return true;
             }
             return false;
         });
@@ -213,14 +183,46 @@ public class MainActivity extends AppCompatActivity {
         b2.setOnTouchListener((v, event) -> {
             int action = event.getAction();
             if (action == MotionEvent.ACTION_DOWN) {
-                b2isPressed = true;
                 gard.setVisibility(View.INVISIBLE);
                 gard3.setVisibility(View.VISIBLE);
+                //for (int i = 0; i < 5; i++) {
+                    float dist = getDist(gard3, bugs_right[0]);
+                    Log.e("asd", "DISTANCE 2 = " + dist);
+                    if (dist < 150 && dist > 100) {
+                        Log.e("asd", "GOOD");
+                        //int finalI1 = i;
+                        bugs_right[0].setVisibility(View.INVISIBLE);
+                        t2.setVisibility(View.VISIBLE);
+                        score_value += 5;
+                        score.setText(String.valueOf(score_value));
+                    } else if (dist <= 100) {
+                        Log.e("asd", "EXCELLENT");
+                        //int finalI = i;
+                        bugs_right[0].setVisibility(View.INVISIBLE);
+                        t1.setVisibility(View.VISIBLE);
+                        score_value += 10;
+                        score.setText(String.valueOf(score_value));
+                    } else if (dist >= 150) {
+                        Log.e("asd", "MISS");
+                        t3.setVisibility(View.VISIBLE);
+                        life_value -= 1;
+                        life.setText(String.valueOf(life_value));
+                    }
+
+                boolean isAttacking = getDist(bugs_right[0], plant2) < 10;
+                if (isAttacking) {
+                    life_value--;
+                    life.setText(String.valueOf(life_value));
+                }
+                //}
                 return true;
             } else if (action == MotionEvent.ACTION_UP) {
-                b2isPressed = false;
                 gard3.setVisibility(View.INVISIBLE);
                 gard.setVisibility(View.VISIBLE);
+                t1.setVisibility(View.INVISIBLE);
+                t2.setVisibility(View.INVISIBLE);
+                t3.setVisibility(View.INVISIBLE);
+                return true;
             }
             return false;
         });
@@ -229,11 +231,7 @@ public class MainActivity extends AppCompatActivity {
 
     /* fonction qui calcule la distance entre deux images (instances de type View) à l'aide des coordonnées du centre de chaque image */
     public float getDist(View v1, View v2) {
-        float x1 = v1.getWidth() / 2;
-        float y1 = v1.getHeight() / 2;
-        float x2 = v2.getWidth() / 2;
-        float y2 = v2.getHeight() / 2;
-        return (float) Math.sqrt((y1 - y2) * (y1 - y2) + (x1 - x2) * (x1 - x2));
+        return (float) Math.sqrt((v1.getY() - v2.getY()) * (v1.getY() - v2.getY()) + (v1.getX() - v2.getX()) * (v1.getX() - v2.getX()));
     }
 
     @Override
